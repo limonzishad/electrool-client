9e2f19aec6137f0cc7c461c033da0b11a4adcfd6
limonzishad <zishadlimon@gmail.com>
2022-05-23 20:59:48 +0600

    products & purchase updated

--- src/components/pages/Products/Product.js
+++ src/components/pages/Products/Product.js
@@ -2,24 +2,31 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Product = ({ product }) => {
-    const { _id, name, description, img, pricePerUnit, quantity, availability } = product;
+    const { _id, name, description, img, pricePerUnit, quantity, availability, ratings } = product;
     const navigate = useNavigate();
     const showProductDetails = (id) => {
         navigate(`/purchase/${id}`);
     };
     return (
-        <div className="card card-compact w-96 mx-auto my-5 bg-base-100 shadow-xl">
-            <figure><img src={img} alt={name} /></figure>
+        <div className="card card-compact my-10 bg-base-100 shadow-2xl">
+            <img className="object-cover" src={img} alt={name} />
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p className="">Description: {description}</p>
-                <p className="">Stock: {quantity}</p>
-                {/* <p className="">Price: {pricePerUnit}</p> */}
+                <h2 className="text-xl font-medium">{name}</h2>
+                <p className="font-medium text-justify">Description: {description}</p>
+                <p className="font-medium">Quantity: <span className="text-secondary">{quantity}</span></p>
+                <p className="font-medium">Price per Unit: <span className="text-secondary text-xl">${pricePerUnit}</span></p>
+                <p className="font-medium">Available: <span className="text-black text-lg">{availability ? "Yes" : "No"}</span></p>
+                <p className="font-medium text-center">Ratings: <span className="text-black text-lg">{ratings}</span></p>
                 <div className="card-actions justify-end">
-                    <button onClick={() => { showProductDetails(_id) }} className="w-full mx-auto btn btn-primary">PURCHASE</button>
+                    {
+                        availability ?
+                            <button onClick={() => { showProductDetails(_id) }} className="w-full mx-auto btn btn-primary">ORDER</button>
+                            :
+                            <button className="w-full mx-auto btn btn-primary cursor-not-allowed">ORDER</button>
+                    }
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/pages/Purchase/Purchase.js
+++ src/components/pages/Purchase/Purchase.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 const Purchase = () => {
     const { id } = useParams();
     const [product, setProduct] = useState({});
+    const { _id, name, img, description, pricePerUnit, quantity, availability, ratings } = product;
     useEffect(() => {
         const url = `http://localhost:5000/purchase/${id}`;
         fetch(url)
@@ -12,14 +13,22 @@ const Purchase = () => {
     });
 
     return (
-        <div>
+        <div className="bg-base-200">
             <h1>Purchase</h1>
-            <div className="cards-container">
-                <img src={product.img} alt={product.name} />
-                <h5 className="mt-4">{product.name}</h5>
+            <div className="card card-compact w-8/12 lg:w-6/12 mx-auto my-10 bg-base-100 shadow-2xl">
+                <img className="object-cover" src={img} alt={name} />
+                <div className="card-body">
+                    <h2 className="text-xl font-medium">{name}</h2>
+                    <p className="font-medium text-justify">Description: {description}</p>
+                    <p className="font-medium">Quantity: <span className="text-secondary">{quantity}</span></p>
+                    <p className="font-medium">Price per Unit: <span className="text-secondary text-xl">${pricePerUnit}</span></p>
+                    <p className="font-medium">Available: <span className="text-black text-lg">{availability ? "Yes" : "No"}</span></p>
+                    <p className="font-medium text-center">Ratings: <span className="text-black text-lg">{ratings}</span></p>
+                    <div className="card-actions justify-end">
+                        <button className="w-full mx-auto btn btn-primary">PURCHASE</button>
+                    </div>
+                </div>
             </div>
-            <p className="item-text">Model: {product.name}</p>
-            <p className="item-text">Price: {product.quantity}</p>
         </div>
     );
 };
