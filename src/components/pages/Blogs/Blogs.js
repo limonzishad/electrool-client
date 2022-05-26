5b7797719e4ebd521a62e7d410041ae534e5e233
limonzishad <zishadlimon@gmail.com>
2022-05-26 21:02:06 +0600

    portfolio completed.

--- src/components/pages/Blogs/Blogs.js
+++ src/components/pages/Blogs/Blogs.js
@@ -3,7 +3,40 @@ import React from "react";
 const Blogs = () => {
     return (
         <div>
-            <h1>BLOGS</h1>
+            <div tabIndex="0" className="w-40 md:w-72 lg:w-96 mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">How will you improve the performance of a React Application?</div>
+                <div className="collapse-content">
+                    <p className="text-justify">There are many ways available to optimize a react web application. For instance, we should keep components state local where necessary. Besides, we can memoizing react components to prevent unnecessary re-renders. Also, we can split code in react using dynamic import().</p>
+                </div>
+            </div>
+
+            <div tabIndex="0" className="w-40 md:w-72 lg:w-96 mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">What are the different ways to manage a state in a React application?</div>
+                <div className="collapse-content">
+                    <p className="text-justify">Communication State, Data State, Control State, Session State and Location State are the different types of manage state in react application.</p>
+                </div>
+            </div>
+
+            <div tabIndex="0" className="w-40 md:w-72 lg:w-96 mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">How does prototypical inheritance work?</div>
+                <div className="collapse-content">
+                    <p className="text-justify">Every object that has methods and properties contains an internal and hidden property called Prototype. The Prototypal Inheritance is a feature in javascript. It is used to add methods and properties in an objects. It can inherit the properties and methods of another object. Traditionally, in order to get and set the prototype of an object.</p>
+                </div>
+            </div>
+
+            <div tabIndex="0" className="w-40 md:w-72 lg:w-96 mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">What is a unit test? Why should write unit tests?</div>
+                <div className="collapse-content">
+                    <p className="text-justify">Unit testing is a software development process in which the smallest testable parts of an application are individually studied. The smallest testable parts of an application are called units. This testing methodology is done during the development process by the software developers. Unit test saves both money and time. It makes easy the debugging process. Also, It reduce the reuse of code.</p>
+                </div>
+            </div>
+
+            <div tabIndex="0" className="w-40 md:w-72 lg:w-96 mx-auto collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">Why you do not set the state directly in React?</div>
+                <div className="collapse-content">
+                    <p className="text-justify">It is a convention not to set the state directly even it works in many ways. When you directly set the state, it does not change 'this.state' immediately. Instead of that, it creates a pending state transition, and accessing it after calling this method will only return the present value. Besides, you will lose control of the state over the components.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -3,7 +3,38 @@ import React from "react";
 const Portfolio = () => {
     return (
         <div>
-
+            <div className="card w-52 md:w-96 mx-auto bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">ZISHAD HOSSAIN LIMON</h2>
+                    <hr />
+                    <p className="text-sm md:text-base">Address: Dhaka, Bangladesh</p>
+                    <p className="text-sm md:text-base">Email: zishadlimon@gmail.com</p>
+                    <hr />
+                    <h4 className="font-medium">EDUCATION</h4>
+                    <p className="text-sm md:text-base">BSc. in Computer Science & Engineering</p>
+                    <p className="text-sm md:text-base">American Internation University-Bangladesh</p>
+                    <hr />
+                    <h4 className="font-medium">SKILLS</h4>
+                    <p className="text-sm md:text-base text-justify">HTML5, CSS3, Javascript, React, React Hooks, Firebase, React Firebase Hooks, Express JS, Node JS, MongoDB etc.</p>
+                    <hr />
+                    <h4 className="font-medium">PROJECTS</h4>
+                    <div>
+                        <p className="font-medium"> Handset Store</p>
+                        <p className="text-sm md:text-base text-justify">User can Register and Login to the website using Email address and Google. There are some protected pages which require user Login to visit those pages. User will receive a verification email after successful registration. Besides, user can reset their password.</p>
+                        <p className="font-medium">Live Website: https://handset-store.web.app/</p>
+                    </div>
+                    <div>
+                        <p className="font-medium"> Online Tutor</p>
+                        <p className="text-sm md:text-base text-justify">User can Register and Login to the website using Email address and Google. User will receive a verification email after successful registration. Besides, user can reset their password.</p>
+                        <p className="font-medium">Live Website: https://online-tutor-dba75.web.app/</p>
+                    </div>
+                    <div>
+                        <p className="font-medium"> Laptop Ghor</p>
+                        <p className="text-sm md:text-base text-justify">This is a simple static ui based react application. The dashboard shows dynamic charts.</p>
+                        <p className="font-medium">Live Website: https://product-analysis-website-limonzishad.netlify.app/</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
